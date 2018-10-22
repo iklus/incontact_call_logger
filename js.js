60c971fe7a30058bf20f1cee87bd49dde4107796
unknown <samantha.oakes@vectorsolutions.com>
2018-10-22 13:30:21 -0400

    basic layout

---
+++ js.js
@@ -0,0 +1,21 @@
+function callReasonOnclick(){
+    document.getElementById("callReasonDropdown").classList.toggle("show");
+}
+
+function salesforceOrganizationOnclick(){
+    document.getElementById("salesforceOrganizationDropdown").classList.toggle("show");
+}
+
+function salesforceContactOnclick(){
+    document.getElementById("salesforceContactDropdown").classList.toggle("show");
+}
+
+function phoneNumberDropdownOnclick(){
+    document.getElementById("phoneNumberDropdown").classList.toggle("show");
+}
+
+/*window.onclick = function(event){
+    document.getElementById("salesforceContactDropdown").classList.remove("show");
+    document.getElementById("salesforceOrganizationDropdown").classList.remove("show");
+    document.getElementById("callReasonDropdown").classList.remove("show");
+}*/
