a7cfda41ec84ea547d22163222b9863a48484de9
SamanthaOakes <samantha.oakes@vectorsolutions.com>
2018-10-24 14:24:34 -0400

    phone number dropdown works

--- js.js
+++ js.js
@@ -11,11 +11,29 @@ function salesforceContactOnclick(){
 }
 
 function phoneNumberDropdownOnclick(){
+    let xmlhttp = new XMLHttpRequest();
+    let txt = '';
+    xmlhttp.overrideMimeType("application/json");
+    xmlhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            if (document.getElementById("phoneNumberDropdown").childElementCount < 2){                
+                var response = JSON.parse(this.responseText);
+                console.log(response);
+                response.contactList.contactStates.forEach(function(call){
+                    txt += '<option>' + call.fromAddr + '</option>';
+                });   
+                console.log(txt);
+                document.getElementById("phoneNumberDropdown").innerHTML = txt;
+            }
+        }
+    }
+    xmlhttp.open("GET", 'call_history.json', true);
+    xmlhttp.send(null);
     document.getElementById("phoneNumberDropdown").classList.toggle("show");
 }
 
 /*window.onclick = function(event){
     document.getElementById("salesforceContactDropdown").classList.remove("show");
-    document.getElementById("salesforceOrganizationDropdown").classList.remove("show");
+    document.getElementById("ssalesforceOrganizationDropdown").classList.remove("show");
     document.getElementById("callReasonDropdown").classList.remove("show");
 }*/
