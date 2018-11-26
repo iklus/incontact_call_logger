c9569eec56b76a51ed3656baf85a9539abec0ae4
Ivan Klus <git@ivankl.us>
2018-11-26 00:34:39 -0500

    Moved URLParams func

--- js/incontact.js
+++ js/incontact.js
@@ -21,17 +21,6 @@ function redirectToInContactAuth() {
 }
 
 var urlParams;
-(window.onpopstate = function () {
-    var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
-        urlParams = {};
-    while (match = search.exec(query))
-        urlParams[decode(match[1])] = decode(match[2]);
-})();
-console.log(urlParams);
 
 function getAgentContactHistory() {
     var agentId = document.getElementById('agentIdValue').value;
